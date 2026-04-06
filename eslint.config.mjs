1b1141843e100344b88851a5635da2943f957c56
Abourass <Abourassa@AssetVal.com>
2026-04-06 12:09:48 -0600

    refactor: update ESLint configuration to use recommended TypeScript settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,26 @@
 // @ts-check
 import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
-import tseslint from 'typescript-eslint';
+import * as eslintConfig from 'eslint/config';
+import ts from 'typescript-eslint';
 
-export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.recommendedTypeChecked,
+const typeCheckedConfig = ts.configs.recommendedTypeChecked.map((config) => ({
+  ...config,
+  files: ['src/**/*.ts', 'vite.config.ts'],
+}));
+
+export default eslintConfig.defineConfig(
   {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  eslint.configs.recommended,
+  ...typeCheckedConfig,
+  {
+    files: ['src/**/*.ts', 'vite.config.ts'],
     languageOptions: {
       parserOptions: {
-        projectService: true,
+        projectService: {
+          allowDefaultProject: ['vite.config.ts'],
+        },
       },
     },
   },

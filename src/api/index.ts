c5cf2c2a464ea586fc875819fa53874c653bb4a7
Abourass <Abourassa@AssetVal.com>
2026-04-06 13:28:58 -0600

    feat: export DiceFactory, CORE_COLORSETS, and TEXTURE_LIST from the dice module

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,3 +5,4 @@
 export { DiceSystem } from './dice-system.js';
 export { DiceSFX } from './dice-sfx.js';
 export type { IDice3D, IDiceFactory, IDiceBox, IDiceSystem, IDiceSFXClass } from './dice3d.js';
+export { DiceFactory, CORE_COLORSETS, TEXTURE_LIST } from '../dice/index.js';

c21bc08f003efced047da0c52b183411dfb71d4c
Antonio Bourassa <Abourassa@AssetVal.com>
2026-04-07 13:49:13 -0600

    feat(dependencies): update '@dimforge/rapier3d-compat' to version 0.19.3 and adjust vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@dimforge/rapier3d': '@dimforge/rapier3d-compat',
+    },
+  },
   test: {
     environment: 'node',
     include: ['tests/**/*.spec.ts'],
     exclude: ['dist/**', 'node_modules/**'],
     fileParallelism: false,
-  },
-  benchmark: {
-    include: ['tests/**/*.bench.ts'],
-    exclude: ['dist/**', 'node_modules/**'],
+    benchmark: {
+      include: ['tests/**/*.bench.ts'],
+      exclude: ['dist/**', 'node_modules/**'],
+    },
   },
 });
